fa08333890396b0b3c2a79f773cd678b2d507774
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-04-08 18:02:41 +0200

    add script

--- js/script.js
+++ js/script.js
@@ -1,39 +1,62 @@
-// Chiedere all'utente i km da percorrere
-const travelKm = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
-console.log(travelKm);
+// Creo il form per chiedere all'utente i km da percorrere e l'età
+const formBtn = document.getElementById("form-btn");
+const userName = document.getElementById("user-name");
+const userKm = document.getElementById("user-km");
+const userAge = document.getElementById("user-age");
 
-// Chiedere all'utente l'età
-const userAge = parseInt(prompt("Quanti anni hai?"));
-console.log(userAge);
+formBtn.addEventListener("click", 
+    function() {
+        // Prelevo gli input dell'utente
+        const name = userName.value;
+        console.log(name);
 
-// Calcolare il prezzo totale del viaggio
-    // -prezzo del biglietto 0.21€ al km
-let priceTiket = travelKm * 0.21;
-console.log(`prezzo base: ${priceTiket}`);
+        const age = userAge.value;
+        console.log(age);
 
-// Calcolare lo sconto in base al passeggero
-let discount;
-let finalPrice;
+        const km = userKm.value;
 
-if (userAge < 18) {
-    discount = (priceTiket * 20 / 100);  
-} else if (userAge > 65) {
-    discount = (priceTiket * 45 / 100);
-} else {
-    discount = 0
-}
-console.log(`sconto: ${discount}`);
- 
-// Sottrare lo sconto al prezzo pieno
-finalPrice = priceTiket - discount;
-console.log(finalPrice);
+        let priceTicket = km * 0.21;
+        // Calcolare lo sconto in base al passeggero
+        let discount;
+        let finalPrice;
 
-// Creare il messaggio da visualizzare
-const message = `Il prezzo del biglietto è di: €${finalPrice.toFixed(2)}`;
-console.log(`prezzo finale: ${finalPrice}`);
+        if (age === 'minorenne') {
+            discount = (priceTicket * 20 / 100);  
+        } else if (age === 'over-65') {
+            discount = (priceTicket * 45 / 100);
+        } else {
+            discount = 0
+        }
+        
+        // Sottrare lo sconto al prezzo pieno
+        finalPrice = priceTicket - discount;
+        console.log('prezzo finale', finalPrice);
 
-// Stampare il prezzo finale in html
-document.getElementById('price').innerHTML = message;
+        // Visualizzo nella pagina le risposte dell'utente
+        // - inserire le risposte negli elementi corrispondenti
+        const userResponse = document.querySelector(".result p");
+        console.log(userResponse);
+        userResponse.innerHTML = `${name}`;
 
+        
+        // // // DA RIFARE USANDO GLI ID SUGLI LI
+        const offering = document.getElementById("offerta");
+        console.log(offering);
+        offering.innerHTML = "standard tiket";
 
-
+        const carriage = document.getElementById("carrozza");
+        console.log(carriage);
+        carriage.innerHTML = Math.floor((Math.random() * 10) + 1);
+        
+        const code = document.getElementById("codice");
+        console.log(code);
+        code.innerHTML = Math.floor((Math.random() * 100000) + 90000);
+        
+        const price =document.getElementById("costo");
+        console.log(price);
+        price.innerHTML = `${finalPrice}`;
+         
+        //  resultContainer.classList.remove("d-none");
+        
+    }
+);
